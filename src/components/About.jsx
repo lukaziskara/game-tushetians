a0f93395c5772eb92ac517c47d2f47f1c2768694
lukaziskara <lukaziskara@gmail.com>
2023-06-21 15:18:44 +0400

    დაჭერისას აღწერები ჩამოიშლება.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+
 export default function About(props) {
+  const [isAboutOpen, setIsAboutOpen] = useState(false);
   console.log(props.desc, "dwadwejkn");
   return (
     <div className="about">
-      <h2>{props.desc.h2}</h2>
-      <p>{props.desc.p}</p>
+      {isAboutOpen ? (
+        <div className="about_open">
+          <h2
+            className="about_header"
+            onClick={() => setIsAboutOpen(!isAboutOpen)}
+          >
+            {props.desc.h2}
+          </h2>
+          <p className="about_paragraph">{props.desc.p}</p>
+        </div>
+      ) : (
+        <div
+          className="about_close"
+          onClick={() => setIsAboutOpen(!isAboutOpen)}
+        >
+          აღწერა
+        </div>
+      )}
     </div>
   );
 }
